1f10d29cb52d3089496c05e2ccf972e1de551d1b
backend <eferj11@gmail.com>
2026-05-15 18:35:52 +0800

    porfolio

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() { 
+    return (
+        <nav className="bg-[#22223b] text-white flex items-center justify-between px-6 py-4 border-b-1 border-black shadow-sm">
+            <div className="text-xl font-bold text-cyan-400">My Portfolio</div>
+            <div className="space-x-4">
+               
+            </div>
+        </nav>
+    );
+}
